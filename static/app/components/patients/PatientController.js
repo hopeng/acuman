0279a9009103fd207b9818707b9d0270e424ea67
hopeng <haopeng.huang@gmail.com>
2016-04-26 00:59:24 +1000

    use normal card and ng-include to show edit page, no sidenav or dialog
    confirm dialog for delete
    
    formated

--- static/app/components/patients/PatientController.js
+++ static/app/components/patients/PatientController.js
@@ -3,22 +3,67 @@
 angular.module('caseManagerApp.patients', ['ngResource'])
 
   .controller('PatientController',
-    function ($resource, $mdMedia, $mdDialog, $mdToast) {
+    function ($resource, $mdMedia, $mdDialog, $mdToast, $log) {
 
       var self = this;
-      this.newCaseInProgress = false;
-      this.newCaseResult = null;
-      this.upSertMode = false;
+      this.upserting = false;
 
-      var patientUpdator = $resource(CONF.URL.PATIENTS + '/:id', null, { 'update': { method:'PUT' } });
+      var patientUpdator = $resource(CONF.URL.PATIENTS + '/:id', null, {'update': {method: 'PUT'}});
       var patientResource = $resource(CONF.URL.PATIENTS + '/:id');
       this.patientList = patientResource.query();
+      // sourced from http://www.privatehealth.gov.au/dynamic/healthfundlist.aspx, todo save in DB
+      this.healthFundList = $resource(CONF.URL.HEALTH_FUNDS).query();
 
-      this.upsertPatient = function (patient) {
+      this.doctor = 'HFANG';
+
+      this.onPatientEdit = function (ev, patient) {
+        this.currentPatient = patient;
+        this.upserting = true;
+      };
+
+      this.onSubmitPatient = function (ev) {
+        upsertPatient(this.currentPatient);
+        this.upserting = false;
+      };
+
+      this.onCancelPatientForm = function (ev) {
+        this.upserting = false;
+      };
+
+      this.onDeletePatient = function (ev) {
+        var confirm = $mdDialog.confirm()
+          .title('Would you like to delete patient ' + this.currentPatient.patientId + '?')
+          .textContent('You cannot undo this action')
+          .ariaLabel('Lucky day')
+          .targetEvent(ev)
+          .ok('DELETE THIS PATIENT')
+          .cancel('CANCEL');
+
+        $mdDialog.show(confirm).then(
+          function () {
+            deletePatient(self.currentPatient);
+            self.upserting = false;
+          });
+      };
+
+      var showToast = function (message) {
+        $mdToast.hide().then(
+          function () {
+            var simpleToast = $mdToast.simple()
+              .textContent(message)
+              .action('OK')
+              .position('top right')
+              .hideDelay(5000);
+            $mdToast.show(simpleToast);
+          }
+        );
+      };
+
+      var upsertPatient = function (patient) {
         console.log("onSubmitPatient", patient);
         var patientId = patient.patientId;
         if (patientId) {
-          patientUpdator.update({ id: patientId }, patient).$promise.then(
+          patientUpdator.update({id: patientId}, patient).$promise.then(
             function (response) {
               showToast('Successfully updated patient ' + patientId);
 
@@ -41,9 +86,9 @@ angular.module('caseManagerApp.patients', ['ngResource'])
         }
       };
 
-      this.deletePatient = function (patient) {
+      var deletePatient = function (patient) {
         var id = patient.patientId;
-        patientResource.delete({ id: id}).$promise.then(
+        patientResource.delete({id: id}).$promise.then(
           function () {
             var index = self.patientList.indexOf(patient);
             self.patientList.splice(index, 1);
@@ -54,70 +99,4 @@ angular.module('caseManagerApp.patients', ['ngResource'])
           }
         );
       };
-
-      var showToast = function (message) {
-        $mdToast.hide().then(
-          function () {
-            var simpleToast = $mdToast.simple()
-              .textContent(message)
-              .action('OK')
-              .position('top right')
-              .hideDelay(5000);
-            $mdToast.show(simpleToast);
-          }
-        );
-      };
-
-      this.onPatientEdit = function(ev, patient) {
-        var useFullScreen = $mdMedia('sm') || $mdMedia('xs');
-        $mdDialog.show({
-          controller: EditPatientController,
-          controllerAs: 'svc',
-          templateUrl: 'app/components/patients/edit-patient.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          fullscreen: useFullScreen,
-          locals: {
-            currentPatient: patient
-          }
-        })
-          .then(function(response) {
-            if (response.action === 'insert') {
-              self.upsertPatient(response.patient);
-
-            } else if (response.action === 'delete') {
-              self.deletePatient(response.patient);
-
-            } else {
-              console.log('unknown action ', response.action, '. patient ', response.patient);
-            }
-
-          }, function() {
-            // 'You cancelled the dialog.'
-          });
-      };
-
-      function EditPatientController ($resource, $mdDialog, currentPatient) {
-        var editCtrlSelf = this;
-        editCtrlSelf.currentPatient = currentPatient;
-
-        // sourced from http://www.privatehealth.gov.au/dynamic/healthfundlist.aspx, todo save in DB
-        editCtrlSelf.healthFundList = $resource(CONF.URL.HEALTH_FUNDS).query();
-
-        editCtrlSelf.onSubmitEditPatient = function() {
-          $mdDialog.hide({ patient: editCtrlSelf.currentPatient, action: 'insert' });
-        };
-
-        editCtrlSelf.onCancelDialog = function() {
-          $mdDialog.cancel();
-        };
-        
-        editCtrlSelf.onClearPatientForm = function () {
-          editCtrlSelf.currentPatient = {};
-        };
-
-        editCtrlSelf.onDeletePatient = function () {
-          $mdDialog.hide({ patient: editCtrlSelf.currentPatient, action: 'delete' });
-        };
-      }
     });
