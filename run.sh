9e7d7d0a36507c88e0bd276899672dbc94f98479
Ubuntu <ubuntu@ip-172-31-35-116.ap-southeast-2.compute.internal>
2017-05-02 09:38:15 +0000

    added routing to port 80

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ then
     ./gradlew clean build
 fi
 
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 4567
+
+
 nohup java -jar build/libs/acuman-all.jar >> server.log 2>&1 &
