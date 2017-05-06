6ff10b982508ab62a6b72b76e0e8969d9d9c178e
hopeng <haopeng.huang@gmail.com>
2017-05-06 17:59:18 +1000

    upgraded js-xlsx now works with wiredep

--- static/app/bower_components/js-xlsx/tests/write.js
+++ static/app/bower_components/js-xlsx/tests/write.js
@@ -6,18 +6,27 @@ var data = [
 	[1,2,3],
 	[true, false, null, "sheetjs"],
 	["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"],
-	["baz", null, "qux"]
+	["baz", null, "qux", 3.14159],
+	["hidden"],
+	["visible"]
 ];
 
 var ws_name = "SheetJS";
 
 var wscols = [
-	{wch:6},
-	{wch:7},
+	{wch:6}, // "characters"
+	{wpx:50}, // "pixels"
 	{wch:10},
-	{wch:20}
+	{wpx:125}
 ];
 
+var wsrows = [];
+wsrows[0] = {hpt: 12}; // "points"
+wsrows[1] = {hpx: 16}; // "pixels"
+wsrows[2] = {hpt: 18};
+wsrows[3] = {hpx: 24};
+wsrows[4] = {hidden:true}; // hide row
+wsrows[5] = {hidden:false};
 
 console.log("Sheet Name: " + ws_name);
 console.log("Data: "); for(var i=0; i!=data.length; ++i) console.log(data[i]);
@@ -28,60 +37,108 @@ console.log("Columns :"); for(i=0; i!=wscols.length;++i) console.log(wscols[i]);
 /* require XLSX */
 if(typeof XLSX === "undefined") { try { XLSX = require('./'); } catch(e) { XLSX = require('../'); } }
 
-/* dummy workbook constructor */
-function Workbook() {
-	if(!(this instanceof Workbook)) return new Workbook();
-	this.SheetNames = [];
-	this.Sheets = {};
-}
-var wb = new Workbook();
-
-
-/* TODO: date1904 logic */
-function datenum(v, date1904) {
-	if(date1904) v+=1462;
-	var epoch = Date.parse(v);
-	return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
-}
+/* blank workbook constructor */
+var wb = { SheetNames: [], Sheets: {} };
 
 /* convert an array of arrays in JS to a CSF spreadsheet */
-function sheet_from_array_of_arrays(data, opts) {
-	var ws = {};
-	var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
-	for(var R = 0; R != data.length; ++R) {
-		for(var C = 0; C != data[R].length; ++C) {
-			if(range.s.r > R) range.s.r = R;
-			if(range.s.c > C) range.s.c = C;
-			if(range.e.r < R) range.e.r = R;
-			if(range.e.c < C) range.e.c = C;
-			var cell = {v: data[R][C] };
-			if(cell.v == null) continue;
-			var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
-
-			/* TEST: proper cell types and value handling */
-			if(typeof cell.v === 'number') cell.t = 'n';
-			else if(typeof cell.v === 'boolean') cell.t = 'b';
-			else if(cell.v instanceof Date) {
-				cell.t = 'n'; cell.z = XLSX.SSF._table[14];
-				cell.v = datenum(cell.v);
-			}
-			else cell.t = 's';
-			ws[cell_ref] = cell;
-		}
-	}
-
-	/* TEST: proper range */
-	if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
-	return ws;
-}
-var ws = sheet_from_array_of_arrays(data);
+var ws = XLSX.utils.aoa_to_sheet(data, {cellDates:true, dense:false});
 
 /* TEST: add worksheet to workbook */
 wb.SheetNames.push(ws_name);
 wb.Sheets[ws_name] = ws;
 
-/* TEST: column widths */
+/* TEST: simple formula */
+ws['C1'].f = "A1+B1";
+ws['C2'] = {t:'n', f:"A1+B1"};
+
+/* TEST: single-cell array formula */
+ws['D1'] = {t:'n', f:"SUM(A1:C1*A1:C1)", F:"D1:D1"};
+
+/* TEST: multi-cell array formula */
+ws['E1'] = {t:'n', f:"TRANSPOSE(A1:D1)", F:"E1:E4"};
+ws['E2'] = {t:'n', F:"E1:E4"};
+ws['E3'] = {t:'n', F:"E1:E4"};
+ws['E4'] = {t:'n', F:"E1:E4"};
+ws["!ref"] = "A1:E6";
+
+/* TEST: column props */
 ws['!cols'] = wscols;
 
+/* TEST: row props */
+ws['!rows'] = wsrows;
+
+/* TEST: hyperlink note: Excel does not automatically style hyperlinks */
+ws['A3'].l = { Target: "http://sheetjs.com", Tooltip: "Visit us <SheetJS.com!>" };
+
+/* TEST: built-in format */
+ws['B1'].z = "0%"; // Format Code 9
+
+/* TEST: custom format */
+//ws['B2'].z = "0.0"; wb.SSF[60] = "0.0"; // Custom
+
+/* TEST: page margins */
+ws['!margins'] =  { left:1.0, right:1.0, top:1.0, bottom:1.0, header:0.5, footer:0.5 };
+
+console.log("JSON Data:");console.log(XLSX.utils.sheet_to_json(ws, {header:1}));
+
+/* TEST: hidden sheets */
+wb.SheetNames.push("Hidden");
+wb.Sheets["Hidden"] = XLSX.utils.aoa_to_sheet(["Hidden".split(""), [1,2,3]]);
+wb.Workbook = {Sheets:[]};
+wb.Workbook.Sheets[1] = {Hidden:1};
+
+/* TEST: properties */
+wb.Props = {
+	Title: "SheetJS Test",
+	Subject: "Tests",
+	Author: "Devs at SheetJS",
+	Manager: "Sheet Manager",
+	Company: "SheetJS",
+	Category: "Experimentation",
+	Keywords: "Test",
+	Comments: "Nothing to say here",
+	LastAuthor: "Not SheetJS",
+	CreatedDate: new Date(2017,1,19)
+}
+
+/* TEST: comments */
+ws['A4'].c = [];
+ws['A4'].c.push({a:"SheetJS",t:"I'm a little comment, short and stout!\n\nWell, Stout may be the wrong word"});
+
+/* TEST: sheet protection */
+ws['!protect'] = {
+	password:"password",
+	objects:1,
+	scenarios:1
+};
+
+console.log("Worksheet Model:")
+console.log(ws);
+
 /* write file */
-XLSX.writeFile(wb, 'sheetjs.xlsx');
+XLSX.writeFile(wb, 'sheetjs.xlsx', {bookSST:true});
+XLSX.writeFile(wb, 'sheetjs.xlsm');
+XLSX.writeFile(wb, 'sheetjs.xlsb'); // no formula
+XLSX.writeFile(wb, 'sheetjs.xls', {bookType:'biff2'}); // no formula
+XLSX.writeFile(wb, 'sheetjs.xml.xls', {bookType:'xlml'});
+XLSX.writeFile(wb, 'sheetjs.ods');
+XLSX.writeFile(wb, 'sheetjs.fods');
+XLSX.writeFile(wb, 'sheetjs.slk');
+XLSX.writeFile(wb, 'sheetjs.csv');
+XLSX.writeFile(wb, 'sheetjs.txt');
+XLSX.writeFile(wb, 'sheetjs.prn');
+XLSX.writeFile(wb, 'sheetjs.dif');
+
+/* test by reading back files */
+XLSX.readFile('sheetjs.xlsx');
+XLSX.readFile('sheetjs.xlsm');
+XLSX.readFile('sheetjs.xlsb');
+XLSX.readFile('sheetjs.xls');
+XLSX.readFile('sheetjs.xml.xls');
+XLSX.readFile('sheetjs.ods');
+XLSX.readFile('sheetjs.fods');
+XLSX.readFile('sheetjs.slk');
+XLSX.readFile('sheetjs.csv');
+XLSX.readFile('sheetjs.txt');
+XLSX.readFile('sheetjs.prn');
+XLSX.readFile('sheetjs.dif');
